9839f7e3536d16c68472ab07cc1b7e9206d966f0
jdewinne <joris.dewinne@gmail.com>
2016-03-10 16:25:26 -0800

    Added execute script on device

---
+++ src/main/resources/perfectomobile/ExecuteScript.py
@@ -0,0 +1,20 @@
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+#
+
+from com.perfectomobile.httpclient import Credentials
+from com.perfectomobile.httpclient.execution import ExecutionsHttpClient
+
+
+credentials = Credentials(perfectomobileServer['username'], perfectomobileServer['password'])
+client = ExecutionsHttpClient(perfectomobileServer['url'], credentials)
+try:
+    scrip_execution_result = client.executeScript(deviceId, scriptKey)
+    executionId = scrip_execution_result.getExecutionId()
+except:
+    print "Failed to Execute script.\n"
+    raise
+
+
