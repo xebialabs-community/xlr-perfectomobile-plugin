3e9b0c35259e16a79f9f6a589ff5d90580fe0398
jdewinne <joris.dewinne@gmail.com>
2016-03-14 17:45:45 -0700

    Added wait for execution task

--- src/main/resources/perfectomobile/WaitForExection.py
+++ src/main/resources/perfectomobile/WaitForExection.py
@@ -11,18 +11,13 @@ from com.perfectomobile.httpclient.execution import ExecutionsHttpClient
 credentials = Credentials(perfectomobileServer['username'], perfectomobileServer['password'])
 client = ExecutionsHttpClient(perfectomobileServer['url'], credentials)
 
-executionIds = {}
 executionStatuses = {}
 try:
-    for key in deviceIds:
-        scrip_execution_result = client.executeScript(key, scriptKey)
-        executionIds[key] = scrip_execution_result.getExecutionId()
-    if waitForExecutionFinish:
-        for key, value in executionIds.iteritems():
-            response = client.waitForExecution(value)
-            executionStatuses[value] = response.get("status")
+    for key, value in executionIds.iteritems():
+        response = client.waitForExecution(value)
+        executionStatuses[value] = response.get("status")
 except:
-    print "Failed to Execute script.\n"
+    print "Failed to get execution statuses.\n"
     raise
 
 
